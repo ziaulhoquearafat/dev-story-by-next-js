aaca5e1cc261442a3a56bed74a62ead56de2c864
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-02-21 01:53:46 +0600

    create next js environment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,3 @@
-import Banner from "@/components/Banner";
-
 export default function Home() {
-  return (
-    <div>
-      <Banner />
-    </div>
-  );
+  return <div>Hello World</div>;
 }
--- src/app/stories/[id]/loading.jsx
+++
@@ -1,9 +0,0 @@
-const loading = () => {
-  return (
-    <div className="min-h-screen flex gap-5 text-8xl justify-center items-center">
-      <h2>L</h2> <span className="animate-spin">😎</span> <h2>ADING STORIES</h2>
-    </div>
-  );
-};
-
-export default loading;
